0bb6cbafb558421de1aa2757d44afc72ee5c9757
Derick Hooley <dghooley@gmail.com>
2021-12-07 22:03:43 -0600

    offline functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+// const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require('path');
-const webpack = require("webpack");
-// const { webpack } = require('webpack');
-module.exports = {
+
+const config = {
     entry: {
-        app: './assets/js/script.js', 
+        app: './assets/js/script.js',
         events: "./assets/js/events.js",
         schedule: "./assets/js/schedule.js",
         tickets: "./assets/js/tickets.js",
@@ -16,16 +18,16 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jpg$/i,
+                test: /\.(png|jpe?g|gif)$/i,
                 use: [
                     {
                         loader: 'file-loader',
                         options: {
                             esModule: false,
-                            name (file) {
+                            name(file) {
                                 return "[path][name].[ext]"
                             },
-                            publicPath: function(url) {
+                            publicPath: function (url) {
                                 return url.replace("../", "/assets/")
                             }
                         }
@@ -45,6 +47,22 @@ module.exports = {
         new BundleAnalyzerPlugin({
             analyzerMode: "static", // the report ouputs to an HTML file in the dist folder
         })
+        // new WebpackPwaManifest({
+        //   name: "Food Event",
+        //   short_name: "Foodies",
+        //   description: "An app that allows you to view upcoming food events.",
+        //   background_color: "#01579b",
+        //   theme_color: "#ffffff",
+        //   fingerprints: false,
+        //   inject: false,
+        //   icons: [{
+        //     src: path.resolve("assets/img/icons/icon-512x512.png"),
+        //     sizes: [96, 128, 192, 256, 384, 512],
+        //     destination: path.join("assets", "icons")
+        //   }]
+        // })
     ],
     mode: 'development'
-}
+};
+
+module.exports = config;
